ce8eecebb1d2df07d0387f81335cd2976be9c445
kiriiu <maxim998.911@gmail.com>
2025-04-16 20:52:23 +0300

    Update models.go

--- internal/database/models.go
+++ internal/database/models.go
@@ -9,5 +9,5 @@ type File struct {
 	gorm.Model
 	FilePath string    `json:"file_path"`
 	FileName string    `json:"file_name"`
-	UUID     uuid.UUID `json:"uuid"@gmail.com@`
+	UUID     uuid.UUID `json:"uuid"`
 }
